5700864fdc1f0e10efc4ddbb0352691ea4e181a4
luigi1997 <luigi1997@live.com.pt>
2018-03-10 15:07:22 +0000

    PP ficha 4

---
+++ PP/FIcha 4/JavaApplication56_Caterpie/src/javaapplication56_caterpie/JavaApplication56_Caterpie.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication56_caterpie;
+
+/**
+ *
+ * @author Luís Pereira
+ */
+public class JavaApplication56_Caterpie {
+
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.id[0] = 'A';
+        user1.id[1] = 'B';
+        user1.id[2] = 'C';
+        user1.name = new char[]{'B', 'r', 'u', 'n', 'o'};
+        user1.email = new char[]{'b', 'm', 'o', '@', 'e', 's', 't', 'g', 'f', '.', 'i', 'p', 'p', '.', 'p', 't'};
+    }
+
+}
