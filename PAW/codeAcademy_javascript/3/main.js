23955d59ebd1eb2765daf69e9b1a74502f975353
luigi1997 <luigi1997@live.com.pt>
2018-03-19 19:38:00 +0000

    3

--- PAW/codeAcademy_javascript/3/main.js
+++
@@ -1,9 +0,0 @@
-//temperatura em kelvins
-let kelvin = 0;
-//celsius = kelvin - 273
-let celsius = kelvin - 273;
-//fahrenheit = celsius * (9/5) + 32
-let fahrenheit = Math.floor(celsius * (9/5) + 32);
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
-let newton = Math.floor(celsius * (33/100));
-console.log(newton);
