baf2116efe6dd358f1fee9e834e5ee0b657829c0
luigi1997 <luigi1997@live.com.pt>
2018-03-19 20:40:08 +0000

    3.3

--- PAW/codeAcademy_javascript/3/main.js
+++ PAW/codeAcademy_javascript/3/main.js
@@ -1,7 +1,16 @@
-let isSoccerFan = false;
+let wordCount = 1;
 
-if (isSoccerFan){
-  console.log('Goal!');
+if (wordCount) {
+  console.log("Great! You've started your work!");
 } else {
-	console.log('No goal!');
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'oi';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
 }
